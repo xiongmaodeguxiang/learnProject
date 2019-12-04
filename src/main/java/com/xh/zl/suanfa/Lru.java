5b50186a15c89b15b53edfd8b013bef8ffa274d8
张林 <15117924335@163.com>
2019-12-04 15:29:49 +0800

    项目初始化

---
+++ src/main/java/com/xh/zl/suanfa/Lru.java
@@ -0,0 +1,22 @@
+package com.xh.zl.suanfa;
+
+/**
+ * @Author:zl
+ * @Description 基于链表实现LRU算法（Least Recently Used） 最近最久未使用
+ * @Date: 2019/12/4 15:05
+ */
+public class Lru {
+
+    
+    /**
+     * 链表使用节点Node
+     */
+   private static class Node{
+        Object key;
+        Object value;
+        Node pre;
+        Node next;
+    }
+}
+
+
